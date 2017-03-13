1b2b4b41618c0544bbe305a4b771dfe284e7835b
冯陈诚 <523246656@qq.com>
2017-03-13 19:28:37 +0800

    2017.3.13 finnal

--- 2017.3.9/js/index.js
+++ 2017.3.9/js/index.js
@@ -9,10 +9,10 @@ iHomed("config", CONFIG);
 iHomed("api", {
     get_series_list: globalDnsConfigVar.dtvAddr + "/media/series/get_list"
 });
-
 /**
  * 数据获取与分页
  */
+
 var options = {
     request: {
         url: "get_series_list",
@@ -26,8 +26,26 @@ var options = {
             pagenum: 8
         },
         error: function () {
-            console.log("error");
-            alert("服务器异常");
+            console.log(1);
+            $.alert({
+                width: "280px",
+                height: "150px",
+                bindEsc: true,
+                header: false,
+                content: {
+                    height: "115px",
+                    icon: "imgs/icon.png",
+                    html: "服务器异常，请稍后重试"
+                },
+                footer: {
+                    buttons: [{
+                        width: "100%",
+                        buttonText: "确定",
+                        buttonID: "sure",
+                        callback: function () {}
+                    }]
+                }
+            });
         }
     },
     select: {
@@ -79,7 +97,25 @@ var options = {
                 map: {
                     "tool-button1": {
                         action: function () {
-                            alert("点击了按钮1");
+                            $.alert({
+                                width: "280px",
+                                height: "150px",
+                                // bindEsc: true,
+                                header: false,
+                                content: {
+                                    height: "115px",
+                                    icon: "imgs/icon.png",
+                                    html: "点击按钮一"
+                                },
+                                footer: {
+                                    buttons: [{
+                                        width: "100%",
+                                        buttonText: "确定",
+                                        buttonID: "sure",
+                                        callback: function () {}
+                                    }]
+                                }
+                            })
                         },
                         iconClass: {
                             normal: "btn1Normal",
@@ -97,7 +133,18 @@ var options = {
                 map: {
                     "tool-button2": {
                         action: function () {
-                            alert("点击了按钮2");
+                            $.msg({
+                                width: "200px",
+                                height: "70px",
+                                delay: 500,
+                                onClose: false,
+                                content: {
+                                    height: "70px",
+                                    icon: "imgs/icon.png",
+                                    html: "按钮2事件"
+                                },
+                                footer: false
+                            });
                         },
                         iconClass: {
                             normal: "btn2Normal",
@@ -115,7 +162,35 @@ var options = {
                 map: {
                     "tool-button3": {
                         action: function () {
-                            alert("点击了按钮3");
+                            $.content({
+                                zIndex: 1100,
+                                showMask: true,
+                                bindEsc: true,
+                                maskClass: false,
+                                width: "800px",
+                                height: "600px",
+                                animation: "slide",
+                                duration: 300,
+                                header: {
+                                    text: "自定义弹出框",
+                                    height: "30px",
+                                    buttons: [{
+                                        width: "30px",
+                                        buttonText: "关闭",
+                                        buttonBGImg: false,
+                                        buttonId: false,
+                                        callback: false,
+                                    }]
+                                },
+                                content: {
+
+                                    height: "600px",
+                                    src: "http://www.baidu.com",
+                                    frameID: "", // 设置iframe的id属性值
+                                    frameName: "", // 设置iframe的name属性值
+                                },
+                                footer: false
+                            })
                         },
                         iconClass: {
                             normal: "btn3Normal",
@@ -184,29 +259,29 @@ $('#my_table').on('click', '.checkbox', function () {
     if ($this.attr('id') === 'check-all') {
         if ($this.prop('checked')) {
             $tbboxs.prop('checked', true);
-            iHomed( "enableTools", "deleteFile" );
+            iHomed("enableTools", "deleteFile");
         } else {
             $tbboxs.prop('checked', false);
-            iHomed( "disableTools", "deleteFile" );
+            iHomed("disableTools", "deleteFile");
         }
     } else {
         if ($this.prop('checked')) {
             allChecked = true;
-            $tbboxs.each(function () {//判断是否全部选中
+            $tbboxs.each(function () { //判断是否全部选中
                 if ($(this).prop('checked') == false) {
                     allChecked = false;
                 }
             });
-            if($('.checkbox:checked').length >= 1 ){//判断是否有一个选中
-                iHomed( "enableTools", "deleteFile" );
+            if ($('.checkbox:checked').length >= 1) { //判断是否有一个选中
+                iHomed("enableTools", "deleteFile");
             }
             if (allChecked) {
                 $('#check-all').prop('checked', true);
             }
         } else {
             $('#check-all').prop('checked', false);
-            if($('.checkbox:checked').length === 0 ){//判断是否都没有选中
-                iHomed( "disableTools", "deleteFile" );
+            if ($('.checkbox:checked').length === 0) { //判断是否都没有选中
+                iHomed("disableTools", "deleteFile");
             }
         }
     }
@@ -223,7 +298,34 @@ var editBtns = [{
     map: {
         "updataFile": {
             action: function () {
-                alert('上传文件')
+                $.content({
+                    zIndex: 1100,
+                    showMask: true,
+                    bindEsc: true,
+                    maskClass: false,
+                    width: "800px",
+                    height: "600px",
+                    animation: "slide",
+                    duration: 300,
+                    header: {
+                        text: "上传文件",
+                        height: "30px",
+                        buttons: [{
+                            width: "30px",
+                            buttonText: "关闭",
+                            buttonBGImg: false,
+                            buttonId: false,
+                            callback: false,
+                        }]
+                    },
+                    content: {
+                        height: "600px",
+                        src: "uploadfive.html",
+                        frameID: "", // 设置iframe的id属性值
+                        frameName: "", // 设置iframe的name属性值
+                    },
+                    footer: false
+                })
             },
             iconClass: {
                 normal: "updataNomal",
@@ -241,7 +343,30 @@ var editBtns = [{
     map: {
         "deleteFile": {
             action: function () {
-                alert('删除剧集')
+                $.confirm({
+                    width: "280px",
+                    height: "150px",
+                    bindEsc: true,
+                    content: {
+                        height: "115px",
+                        icon: "imgs/icon.png",
+                        html: "删除剧集？"
+                    },
+                    footer: {
+                        padding: "0px",
+                        buttons: [{
+                            width: "50%",
+                            buttonText: "确定",
+                            buttonID: "yes",
+                            callback: function () {}
+                        }, {
+                            width: "50%",
+                            buttonText: "取消",
+                            buttonID: "no",
+                            callback: function () {}
+                        }]
+                    }
+                })
             },
             iconClass: {
                 normal: "deleteNomal",
@@ -253,89 +378,94 @@ var editBtns = [{
 }];
 $('#edit-btn').iHomed('initTools', editBtns);
 var sortBtn = [{
-        type: "select",
-        disabled: false,
-        toolID: "sort-btn",
-        orientation: "horizontal",
-        onInit: function() {
-            $tool = $( this );
-        },
-        // select类型的快捷方式
-        quicker: {
-            text: "",
-            // 可选，点击主按钮时触发哪个子按钮的事件
-            // PS 设置该参数时，按钮会分成两部分，上部分可触发某个子按钮的事件，下部分点击出现子按钮选项
-            mapTo: "tool-option2",
-            quickClass: {
-                normal: "sortMainNormal",
-                hover: "sortMainHover",
-                disabled: "sortMainDisabled"
+    type: "select",
+    disabled: false,
+    toolID: "sort-btn",
+    orientation: "horizontal",
+    onInit: function () {
+        $tool = $(this);
+    },
+    // select类型的快捷方式
+    quicker: {
+        text: "",
+        // 可选，点击主按钮时触发哪个子按钮的事件
+        // PS 设置该参数时，按钮会分成两部分，上部分可触发某个子按钮的事件，下部分点击出现子按钮选项
+        mapTo: "tool-option2",
+        quickClass: {
+            normal: "sortMainNormal",
+            hover: "sortMainHover",
+            disabled: "sortMainDisabled"
+        }
+    },
+    // select类型的子按钮
+    option: [{
+        toolID: "tool-option1",
+        text: "按创建时间"
+    }, {
+        toolID: "tool-option2",
+        text: "按操作时间"
+    }, {
+        toolID: "tool-option3",
+        text: "按名称"
+    }, {
+        toolID: "tool-option4",
+        text: "递增"
+    }, {
+        toolID: "tool-option5",
+        text: "递减"
+    }],
+    // 按钮的事件和图标样式的映射
+    map: {
+        "tool-option1": {
+            action: function () {
+                options.request.data.sortby = 2;
+                $('#page_nav').iHomed('tPager', options);
+            },
+            iconClass: {
+                normal: "opt1Normal",
+                disabled: "opt1Disabled"
             }
         },
-        // select类型的子按钮
-        option: [{
-            toolID: "tool-option1",
-            text: "按创建时间"
-        }, {
-            toolID: "tool-option2",
-            text: "按操作时间"
-        }, {
-            toolID: "tool-option3",
-            text: "按上映时间"
-        }, {
-            toolID: "tool-option4",
-            text: "递增"
-        },{
-            toolID: "tool-option5",
-            text: "递减"
-        }],
-        // 按钮的事件和图标样式的映射
-        map: {
-            "tool-option1": {
-                action: function () {
-                    alert("点击了选项1");
-                },
-                iconClass: {
-                    normal: "opt1Normal",
-                    disabled: "opt1Disabled"
-                }
+        "tool-option2": {
+            action: function () {
+                options.request.data.sortby = 3;
+                $('#page_nav').iHomed('tPager', options);
             },
-            "tool-option2": {
-                action: function () {
-                    alert("点击了选项2");
-                },
-                iconClass: {
-                    normal: "opt2Normal",
-                    disabled: "opt2Disabled"
-                }
+            iconClass: {
+                normal: "opt2Normal",
+                disabled: "opt2Disabled"
+            }
+        },
+        "tool-option3": {
+            action: function () {
+                options.request.data.sortby = 1;
+                $('#page_nav').iHomed('tPager', options);
             },
-            "tool-option3": {
-                action: function () {
-                    alert("点击了选项3");
-                },
-                iconClass: {
-                    normal: "opt3Normal",
-                    disabled: "opt3Disabled"
-                }
+            iconClass: {
+                normal: "opt3Normal",
+                disabled: "opt3Disabled"
+            }
+        },
+        "tool-option4": {
+            action: function () {
+                options.request.data.asc = 1;
+                $('#page_nav').iHomed('tPager', options);
             },
-            "tool-option4": {
-                action: function () {
-                    alert("点击了选项4");
-                },
-                iconClass: {
-                    normal: "opt4Normal",
-                    disabled: "opt4Disabled"
-                }
+            iconClass: {
+                normal: "opt4Normal",
+                disabled: "opt4Disabled"
+            }
+        },
+        "tool-option5": {
+            action: function () {
+                options.request.data.asc = 0;
+                $('#page_nav').iHomed('tPager', options);
             },
-            "tool-option5": {
-                action: function () {
-                    alert("点击了选项5");
-                },
-                iconClass: {
-                    normal: "opt5Normal",
-                    disabled: "opt5Disabled"
-                }
+            iconClass: {
+                normal: "opt5Normal",
+                disabled: "opt5Disabled"
             }
         }
-    }];
-    $('.sort-btn').iHomed('initTools',sortBtn);
+    }
+}];
+$('.sort-btn').iHomed('initTools', sortBtn);
